7c7428944732432abcbf5221908771d1fcfbae4d
sanchitpasricha <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>
2023-04-16 03:24:50 +0530

    basic backend setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+// Cross-Origin Resource Sharing
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
