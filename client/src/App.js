ad5816dbb6c77b95241ee799d743a2639842e83f
sanchitpasricha <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>
2023-04-16 19:22:07 +0530

    listeners on servers and join room function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+
+const socket = io.connect("https://localhost:3000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState("false");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
   return <div className="App"></div>;
 }
 
--- server/index.js
+++ server/index.js
@@ -13,3 +13,24 @@ const io = new Server(server, {
     origin: "http://localhost:3000",
   },
 });
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined Room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
