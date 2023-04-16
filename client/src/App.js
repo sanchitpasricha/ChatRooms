c6536a049fd3e14d601e8d6430a2db331501ddac
sanchitpasricha <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>
2023-04-17 03:35:37 +0530

    Display of messages in chat window

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
+import Chat from "./Chat";
 
 const socket = io.connect("http://localhost:3001");
 
@@ -38,7 +39,9 @@ function App() {
           <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
-        <div></div>
+        <div>
+          <Chat />
+        </div>
       )}
     </div>
   );
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { React, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 
-function Chat() {
-  return <div></div>;
+function Chat({ socket, username, room }) {
+  const [messageList, setMessageList] = useState([]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
