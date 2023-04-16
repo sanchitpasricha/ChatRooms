69153b81476f854971f67d22619e90fade3667fa
sanchitpasricha <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>
2023-04-16 21:21:17 +0530

    connection fix and main page struct

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 
-const socket = io.connect("https://localhost:3000");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState("false");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
@@ -16,7 +16,32 @@ function App() {
     }
   };
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContaier">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="Ella Grace"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room id"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -31,6 +31,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
 });
